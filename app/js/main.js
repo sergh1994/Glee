a696f60d1a40d91dcf48ceaf067703a0fd04c50f
sergh1994 <sergijantoniuk123@gmail.com>
2021-06-11 21:26:11 +0300

    added section: products, design, video-home, article partners

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,22 @@
 $(function (){
-  $('.slider__wrapper').slick({
+  $('.slider-top__wrapper').slick({
     arrows: false,
     dots: true,
     fade: true,
     autoplay: true,
     autoplaySpeed: 3000
   })
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const autoprefixer  = require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
 const imagemin      = require('gulp-imagemin');
 const del           = require('del');
+const fileinclude   = require('gulp-file-include');
 const browserSync   = require('browser-sync').create();
 
 function browsersync() {
@@ -58,16 +59,24 @@ function cleanDist() {
   return del('dist')
 }
 
-function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload)
+ function fileInclude() {
+  return src([
+    'app/html/*.html'
+  ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -76,11 +85,21 @@ function scripts() {
   .pipe(browserSync.stream())
 }
 
+function watching() {
+  watch(['app/html/**/*.html'], fileinclude);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload)
+}
+
 exports.styles       = styles;
 exports.scripts      = scripts;
 exports.browsersync  = browsersync;
 exports.watching     = watching;
 exports.images       = images;
 exports.cleanDist    = cleanDist;
-exports.build        = series(cleanDist, images, build);
-exports.default      = parallel(styles, scripts, browsersync, watching);
+exports.fileinclude  = fileinclude;
+
+
+exports.build        = series(cleanDist, fileinclude, images, build);
+exports.default      = parallel(styles, scripts, fileinclude, browsersync, watching);
